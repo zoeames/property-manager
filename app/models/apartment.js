e570df1c5429f3bcfb02d8c7f60dc0e87fb142fd
Zoe Ames <zoeames@gmail.com>
2014-07-25 00:14:01 -0500

    apartment all room cost added

--- app/models/apartment.js
+++ app/models/apartment.js
@@ -11,13 +11,24 @@ function Apt(unit){
 
 
 Apt.prototype.area = function(){
- var totalArea = 0;
- for(var i=0; i<this.rooms.length; i++){
-  var areaRoom = this.rooms[i].area();
-   totalArea+=areaRoom;
- }
+  var totalArea = 0;
+  for(var i=0; i<this.rooms.length; i++){
+    var areaRoom = this.rooms[i].area();
+    totalArea+=areaRoom;
+    }
   return totalArea;
 };
+
+Apt.prototype.cost = function(){
+  var totalCost = 0;
+  for(var i=0; i<this.rooms.length; i++){
+    var areaCost = this.rooms[i].cost();
+    totalCost+=areaCost;
+    }
+  return totalCost;
+};
+
+
 
 
 module.exports = Apt;
--- test/unit/apartment.js
+++ test/unit/apartment.js
@@ -26,10 +26,21 @@ describe('Apt', function(){
     a1.rooms.push(room2);
     var room3 = new Room('kitchen', 15, 10);
     a1.rooms.push(room3);
-    console.log(a1.rooms);
      expect(a1.area()).to.equal(350);
       expect (a1.rooms.length).to.equal(3);
        });
       });
+  describe('#cost', function(){
+   it('should calcualate the cost of an apt', function(){
+    var a1 = new Apt(a1);
+    var room1 = new Room ('living', 12, 10);
+    a1.rooms.push(room1);
+    var room2 = new Room('bed', 10, 8);
+    a1.rooms.push(room2);
+    var room3 = new Room('kitchen', 15, 10);
+    a1.rooms.push(room3);
+     expect(a1.cost()).to.equal(1750);
+       });
+    });
     });
 
